d92af459e11adc8c8a81cb1b029cb00dd89f9767
unknown <mnmsby4@gmail.com>
2023-11-21 12:37:19 +0530

    add form and scrip file and links

---
+++ script.js
@@ -0,0 +1,70 @@
+const person = {
+    profile_pic: "./image/man19152370.jpg",
+    profile_logo: "./image/man19152370.jpg",
+
+    Name: "Muhammed Siraj",
+    designation: "Graphic Designer",
+    des_link_text: "@NeptuneMark",
+    des_link: "https://neptunemark.com/",
+    contact: "./contact/Haris Husain.vcf",
+};
+
+const contact ={
+    contact_no: "+918089969860",
+    whatsapp: "+918590305060",
+    location: "https://maps.app.goo.gl/zMguVmn3NbNHkdAb7",
+    mail: "neptunemarkindia@gmail.com",
+};
+
+
+
+const links =[
+    facebook={
+    visibil: true,
+    link:"https://neptunemark.com/"
+  },
+    instagram={
+    visibil: true,
+    link:"https://maps.app.goo.gl/zMguVmn3NbNHkdAb7"
+  },
+    skype={
+    visibil: true,
+    link:"https://maps.app.goo.gl/zMguVmn3NbNHkdAb7"
+  },
+    web={
+    visibil: true,
+    link:"https://maps.app.goo.gl/zMguVmn3NbNHkdAb7"
+  },
+    linkedin={
+    visibil: true,
+    link:"https://maps.app.goo.gl/zMguVmn3NbNHkdAb7"
+  },
+    behance={
+    visibil: true,
+    link:"https://maps.app.goo.gl/zMguVmn3NbNHkdAb7"
+  },
+    x={
+    visibil: true,
+    link:"https://maps.app.goo.gl/zMguVmn3NbNHkdAb7"
+  },
+    mail={
+    visibil: false,
+    link:"https://maps.app.goo.gl/zMguVmn3NbNHkdAb7"
+  },
+    google={
+    visibil: true,
+    link:"https://maps.app.goo.gl/zMguVmn3NbNHkdAb7"
+  },
+    telegram={
+    visibil: true,
+    link:"https://maps.app.goo.gl/zMguVmn3NbNHkdAb7"
+  },
+    youtube={
+    visibil: true,
+    link:"https://maps.app.goo.gl/zMguVmn3NbNHkdAb7"
+  },
+    pinterest={
+    visibil: false,
+    link:"https://maps.app.goo.gl/zMguVmn3NbNHkdAb7"
+  },
+];
